9cdab4026b4b37eeb0f981b6a15bf52aa7bb2118
Rohit Bhatt <devrohitbhatt@gmail.com>
2024-01-06 11:14:42 +0530

    small changes

--- Frontend/src/components/Product.tsx
+++ Frontend/src/components/Product.tsx
@@ -5,7 +5,7 @@ const Product = () => {
 	return (
 		<div className='mt-4 md:mt-10'>
 			<h1 className='text-2xl font-bold text-gray-900 lg:text-3xl'>Shop</h1>
-			<div className='grid gap-6 mt-4 md:mt-10 sm:grid-cols-2 lg:grid-cols-4 '>
+			<div className='grid gap-6 mt-6 md:mt-10 sm:grid-cols-2 lg:grid-cols-4 '>
 				<ProductCard />
 				<ProductCard />
 				<ProductCard />
